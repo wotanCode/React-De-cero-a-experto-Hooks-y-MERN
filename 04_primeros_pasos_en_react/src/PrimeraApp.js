9b85a3a5acdd4d63819026fb1dc400cc208b2a60
Pedro Yanez <pedro_y91@outlook.com>
2022-03-04 10:39:17 -0300

    36/retornar elementos del componente

--- 04_primeros_pasos_en_react/src/PrimeraApp.js
+++ 04_primeros_pasos_en_react/src/PrimeraApp.js
@@ -1,7 +1,14 @@
+//import React, { Fragment } from "react";
 import React from "react";
 
 //FC funcional components
 const PrimeraApp = () => {
-  return <h1>Hola mundo</h1>;
-}
+  return (
+    <>
+      <h1>Hola mundo</h1>
+      <h2>Mi primera App</h2>
+    </>
+  );
+};
+
 export default PrimeraApp;
