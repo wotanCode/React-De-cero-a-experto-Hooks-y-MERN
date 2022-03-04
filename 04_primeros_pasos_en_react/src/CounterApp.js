05cd42154e4bcf88347103a0ce18dcaaca3b07e8
Pedro Yanez <pedro_y91@outlook.com>
2022-03-04 13:38:33 -0300

    42 clik

--- 04_primeros_pasos_en_react/src/CounterApp.js
+++ 04_primeros_pasos_en_react/src/CounterApp.js
@@ -1,11 +1,21 @@
 import React from "react";
 import PropTypes from 'prop-types'
 
+
+
 const CounterApp = ( { value } ) => {
+
+  // hadleAdd
+  const handleAdd = (e)=>{
+    console.log(e)
+  }
+
   return(
     <>
       <h1>Counter App</h1>
       <h2> {value} </h2>
+
+      <button onClick={ (e) => {handleAdd} }>+1</button>
     </>
   )
 };
