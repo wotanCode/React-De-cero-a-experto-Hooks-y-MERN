d6813166708d57958db239c10dece3f4ad18ae2d
Pedro Yanez <pedro_y91@outlook.com>
2022-03-04 10:51:34 -0300

    imprimir variables

--- 04_primeros_pasos_en_react/src/PrimeraApp.js
+++ 04_primeros_pasos_en_react/src/PrimeraApp.js
@@ -3,9 +3,22 @@ import React from "react";
 
 //FC funcional components
 const PrimeraApp = () => {
+
+  const saludo = 'hola mundo'; //Valido
+  // const saludo = 123; //Valido
+  // const saludo = 123.2; //Valido
+  // const saludo = true; //inValido
+  // const saludo = [1,2,3,4,5] //Valido
+  // const saludo = {
+  //   nombre: 'hola',
+  //   edad: 31,
+  // }; //valido pero solo si usamos JSON.Stringify
+
   return (
     <>
-      <h1>Hola mundo</h1>
+
+      {/* <pre> { JSON.stringify(saludo, null, 3) } </pre> */}
+      <h1>{saludo}</h1>
       <h2>Mi primera App</h2>
     </>
   );
