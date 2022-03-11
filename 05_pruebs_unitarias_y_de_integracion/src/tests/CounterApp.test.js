bdda7a7e600df77edfdb81867de4f2d3a03a1899
Pedro Yanez <pedro_y91@outlook.com>
2022-03-11 12:50:04 -0300

    64 pruebas con el boton reset

--- 05_pruebs_unitarias_y_de_integracion/src/tests/CounterApp.test.js
+++ 05_pruebs_unitarias_y_de_integracion/src/tests/CounterApp.test.js
@@ -42,4 +42,14 @@ describe('Pruebas en componente counterApp', () => {
     const counterText = wrapper.find('h2').text().trim();
     expect(counterText).toBe('9')
   });
+
+  test('Debe quedar en cero', () =>{
+    const wrapper = shallow(<PrimeraApp value={105}/>)
+    wrapper.find('button').at(0).simulate('click');
+    wrapper.find('button').at(0).simulate('click');
+    wrapper.find('button').at(1).simulate('click');
+    const counterText = wrapper.find('h2').text().trim();
+    
+    expect(counterText).toBe('0')
+  });
 });
