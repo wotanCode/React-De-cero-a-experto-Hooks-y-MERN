61b5a7f1e006e550de154fe3f53d104f0a0ef3cc
Pedro Yanez <pedro_y91@outlook.com>
2022-03-11 10:43:23 -0300

    60 enzyme testint unit

--- 05_pruebs_unitarias_y_de_integracion/src/tests/PrimeraApp.test.js
+++ 05_pruebs_unitarias_y_de_integracion/src/tests/PrimeraApp.test.js
@@ -1,21 +1,28 @@
 import React from "react";
-import { render } from "@testing-library/react";
+//import { render } from "@testing-library/react";
 import PrimeraApp from "../PrimeraApp";
+import {shallow} from 'enzyme'
+import "@testing-library/jest-dom"
+
 
 describe('Pruebas en <Priemrapp />', () => {
-  test('Debe de mostrar el mensaje "Hola soy pedrito"', () =>{
-
-    const saludo = "Hola soy pedrito";
-    const wrapper = render(<PrimeraApp saludo={saludo} />)
+  //test('Debe de mostrar el mensaje "Hola soy pedrito"', () =>{
+    //EN eto tampoco funcionaria en enzy
+    // const saludo = "Hola soy pedrito";
+    // const wrapper = render(<PrimeraApp saludo={saludo} />)
 
     //esto hubiera sido otra forma de hacerlo
     //const {getByText} = render(<PrimeraApp saludo={saludo} />)
     //Abajo hubiera quedado
     //expect( getByText(saludo) )
+    //Esto ya no funciona en enzy
+    //expect( wrapper.getByText(saludo) ).toBeInTheDocument();
 
-    expect( wrapper.getByText(saludo) ).toBeInTheDocument();
+    test('Debe de mostrar <PrimeraApp/> correctamente"', () =>{
+    const saludo = "Hola soy pedrito"
+    const wrapper = shallow(<PrimeraApp saludo= {saludo}/>)
 
-
+    expect( wrapper ).toMatchSnapshot();
 
   })
 })
