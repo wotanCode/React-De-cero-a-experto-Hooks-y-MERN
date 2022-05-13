228196d1919b60a698ddfbe7c64866168825a9fd
Pedro Yanez <pedro_y91@outlook.com>
2022-05-13 10:23:42 -0400

    107/useState

--- 09_Profundizando_en_hooks_generales/src/index.js
+++ 09_Profundizando_en_hooks_generales/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HookApp } from './HookApp';
+// import { HookApp } from './HookApp';
+import { CounterApp } from './components/01-useState/CounterApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HookApp />
+    <CounterApp />
 );
