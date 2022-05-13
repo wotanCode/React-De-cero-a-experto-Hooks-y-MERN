068092a3f91e2b5c3abe10699f4125e92c338b06
Pedro Yanez <pedro_y91@outlook.com>
2022-05-13 13:17:28 -0400

    111/useEffect Precauciones

--- 09_Profundizando_en_hooks_generales/src/components/02-useEffect/Message.js
+++ 09_Profundizando_en_hooks_generales/src/components/02-useEffect/Message.js
@@ -1,18 +1,31 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export const Message = () => {
 
+  const [coors, setCoords] = useState({x:0, y:0})
+  const {x,y} = coors;
+
   useEffect(() => {
 
-    console.log('useEffect incial')
-    return () => {
-      console.log('useEffect final')
+    const mouseMove = (e) => {
+      const coors = {x: e.x, y: e.y}
+      console.log(coors)
+      setCoords(coors)
     }
-  }, [])
+
+    window.addEventListener('mousemove',mouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove',mouseMove);
+    };
+  }, []);
+  
 
   return (
     <div>
       <h3>eres genial!</h3>
+      <h4>x: {x}</h4>
+      <h4>y: {y}</h4>
     </div>
   )
 }
