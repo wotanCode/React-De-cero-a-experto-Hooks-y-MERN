318badaa0358569a252ecac89e812567d7d5cbed
Pedro Yanez <pedro_y91@outlook.com>
2022-05-20 14:01:11 -0400

    114/ useFecth + useCounter

--- 09_Profundizando_en_hooks_generales/src/components/hooks/useFetch.js
+++ 09_Profundizando_en_hooks_generales/src/components/hooks/useFetch.js
@@ -5,6 +5,9 @@ export const useFetch = (url) => {
   const [state, setState] = useState({data: null, loading: true, error: null})
 
   useEffect(() => {
+
+    setState({data: null, loading: true, error: null})
+
     fetch(url)
       .then(response => response.json())
       .then(data => {
