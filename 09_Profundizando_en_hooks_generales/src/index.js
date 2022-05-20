dffbe0d1e3344a6b7817be805b47d3abeb58a184
Pedro Yanez <pedro_y91@outlook.com>
2022-05-20 12:20:30 -0400

    112/formulario custom hook

--- 09_Profundizando_en_hooks_generales/src/index.js
+++ 09_Profundizando_en_hooks_generales/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 // import { HookApp } from './HookApp';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import {CounterWithCustomHook} from './components/01-useState/CounterWithCustomHook';
-import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
+import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <SimpleForm />
+    <FormWithCustomHook />
 );
