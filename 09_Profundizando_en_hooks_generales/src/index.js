e68a8ca26e33ad1262101344b84d2f542a1b2fca
Pedro Yanez <pedro_y91@outlook.com>
2022-05-20 14:49:42 -0400

    115/useRef

--- 09_Profundizando_en_hooks_generales/src/index.js
+++ 09_Profundizando_en_hooks_generales/src/index.js
@@ -6,9 +6,9 @@ import ReactDOM from 'react-dom/client';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook'
 import {MultipleCustomHooks} from './components/03-examples/MultipleCustomHooks';
-
+import { FocusScreen } from './components/04-useRef/FocusScreen';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <MultipleCustomHooks />
+    <FocusScreen />
 );
