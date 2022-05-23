8b6c71575d0fbc898fb62d18e17523ffe6c0a641
Pedro Yanez <pedro_y91@outlook.com>
2022-05-22 22:20:56 -0400

    116 useRef caso real

--- 09_Profundizando_en_hooks_generales/src/components/hooks/useFetch.js
+++ 09_Profundizando_en_hooks_generales/src/components/hooks/useFetch.js
@@ -1,22 +1,47 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export const useFetch = (url) => {
 
+  const isMounted = useRef(true)
+
   const [state, setState] = useState({data: null, loading: true, error: null})
+
+
+  useEffect(() => {
+
+    return () => {
+      isMounted.current = false
+    }
+  }, [])
+
 
   useEffect(() => {
 
     setState({data: null, loading: true, error: null})
 
+
     fetch(url)
       .then(response => response.json())
       .then(data => {
 
-        setState({
-          data,
-          loading: false,
-          error: null
-        })
+
+        setTimeout(() => {
+
+          if (isMounted.current) {
+
+            setState({
+              data,
+              loading: false,
+              error: null
+            });
+          } else {
+            console.log('SetState no se llamo');
+          }
+
+        },2000);
+     
+
+        
       })
       .catch(error => {
         setState({
--- 09_Profundizando_en_hooks_generales/src/index.js
+++ 09_Profundizando_en_hooks_generales/src/index.js
@@ -5,10 +5,11 @@ import ReactDOM from 'react-dom/client';
 // import {CounterWithCustomHook} from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook'
-import {MultipleCustomHooks} from './components/03-examples/MultipleCustomHooks';
-import { FocusScreen } from './components/04-useRef/FocusScreen';
+// import {MultipleCustomHooks} from './components/03-examples/MultipleCustomHooks';
+// import { FocusScreen } from './components/04-useRef/FocusScreen';
+import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <FocusScreen />
+    <RealExampleRef />
 );
