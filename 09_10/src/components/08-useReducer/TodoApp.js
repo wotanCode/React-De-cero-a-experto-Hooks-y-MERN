20f5bc41a0359173e1b7cae60de8345d3e984e37
Pedro Yanez <pedro_y91@outlook.com>
2022-05-27 18:17:29 -0400

    128/creando cascaron en la lista de Todos

--- 09_10/src/components/08-useReducer/TodoApp.js
+++ 09_10/src/components/08-useReducer/TodoApp.js
@@ -4,25 +4,63 @@ import './styles.css';
 
 const initialState = [{
   id: new Date().getTime(),
-  desc: 'Aprender React',
+  desc: 'Comer hamburguesa',
   done: false,
 }]
 
 export const TodoApp = () => {
 
-  const [ todos ] = useReducer(todoReducer, initialState);
+  const [todos] = useReducer(todoReducer, initialState);
 
   return (
     <div>
-      <h1>TodoApp</h1>
-      <hr/>
+      <h1>TodoApp ({todos.length})</h1>
+      <hr />
 
-      <ul>
-        <li>hola</li>
-        <li>hola</li>
-        <li>hola</li>
-        <li>hola</li>
-      </ul>
+      <div className='row'>
+        <div className='col-7'>
+          <ul className='list-group list-group-flush'>
+            {todos.map((todo, idx) => (
+              <li
+                className='list-group-item'
+                key={todo.id}
+              >
+                <p className='text-center'>{idx + 1}. {todo.desc}</p>
+                <button className='btn btn-danger'>Borrar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+
+
+        <div className='col-5'>
+          <h4>Agregar TODO</h4>
+          <hr />
+
+          <form>
+
+            <div className='form-group'>
+              <input
+                type='text'
+                name='description'
+                className='form-control'
+                placeholder='Agregar TODO'
+                autoComplete='off'
+              />
+            </div>
+
+            <button
+              className='btn btn-outline-primary mt-1 btn-block'
+            >
+              Agregar
+            </button>
+
+          </form>
+
+        </div>
+      </div>
+
 
     </div>
   )
